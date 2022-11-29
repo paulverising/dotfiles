ebbe59a11be7d95b63782a94776d32894ac56618
Zach Paul <15836291+paulverising@users.noreply.github.com>
2022-11-28 18:21:34 -0800

    updated typo

--- install.sh
+++ install.sh
@@ -39,7 +39,7 @@ git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:
 brew install neovim
 
 #Stow dotfiles
-if test ~/.gitconfig; then mv ~/.gitconfig ~./gitconfig-bak; fi
+if test ~/.gitconfig; then mv ~/.gitconfig ~/.gitconfig-bak; fi
 stow git
 if test ~/.config/nvim; then mv ~/.config/nvim ~/.config/nvim-bak; fi
 stow nvim
