d523c4f3c78034659d931f7115b1a038768fa628
Zach Paul <15836291+paulverising@users.noreply.github.com>
2022-12-01 08:31:07 -0800

    Started the process of updating my neovim keybindings

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,3 +1,4 @@
+require("zach.plugins-setup")
 require("zach.core.options")
 require("zach.core.keymaps")
 require("zach.core.colorscheme")
---
+++ nvim/.config/nvim/lua/zach/core/keymaps.lua
@@ -0,0 +1,18 @@
+vim.g.mapleader = " "
+
+local keymap = vim.keymap
+
+-- general 
+keymap.set("i", "jk", "<ESC>")
+
+keymap.set("n", "<leader>nh", ":nohl<CR>")
+
+keymap.set("n", "x", '"_x')
+
+keymap.set("n", "<leader>+", "<C-a>")
+keymap.set("n", "<leader>-", "<C-x>")
+
+keymap.set("n", "<leader>sv", "<C-w>v") --split window vertically
+keymap.set("n", "<leader>sh", "<C-w>s") --split window horizontally
+keymap.set("n", "<leader>se", "<C-w>=") --make split windows equal width
+keymap.set("n", "<leader>sx", ":close<CR>") --close curent split window
